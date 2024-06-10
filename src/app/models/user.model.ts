e8cf6b5759e1d45828a115fa0e2d339ec5d70655
anidaduharkic <anida.duharkic@stu.ssst.edu.ba>
2024-06-10 21:01:41 +0200

    configured the authentication and authorization, updated the orders component, and items component.

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+export interface UserModel {
+  id: number
+  fullName: string,
+  phoneNumber: string,
+  email: string,
+  universityName: string,
+  universityYear: string,
+  shortBio: string,
+  linkedinUrl: string,
+  profileImage: string,
+  gpa: number
+}
